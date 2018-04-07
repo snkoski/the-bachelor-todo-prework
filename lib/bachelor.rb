4d13834c7cb945d45735a008386485d45aab098d
Shawn Koski <shawnnkoski@gmail.com>
2018-04-07 01:10:57 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,19 @@
 
 
 def get_first_name_of_season_winner(data, season)
-  
+  data.each do |season_number, contestant_data|
+    if season_number.to_s == season
+      contestant_data.each do |contestant|
+        if contestant[:status] == "Winner"
+          first_name = contestant[:name].split
+          #binding.pry
+          first_name2 = first_name[0]
+          puts first_name2
+          return first_name2.to_s
+        end
+      end
+    end
+  end
 end
 
 
