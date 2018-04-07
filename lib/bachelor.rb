f7ff243ffde9537cc8ff2f4db767c35cd29ed70d
Shawn Koski <shawnnkoski@gmail.com>
2018-04-07 00:26:02 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,40 @@
 require 'pry'
 
+data = { 
+  "season 30": [
+    {
+      "name":      "Beth Smalls",
+      "age":       "26",
+      "hometown":  "Great Falls, Virginia",
+      "occupation":"Nanny/Freelance Journalist",
+      "status":    "Winner"
+    },
+    {
+      "name":       "Becca Tilley",
+      "age":        "27",
+      "hometown":   "Shreveport, Louisiana",
+      "occupation": "Chiropractic Assistant",
+      "status":     "Eliminated Week 8"
+    }
+  ],
+  "season 29": [
+    {
+      "name":      "Ashley Yeats",
+      "age":       "24",
+      "hometown":  "Denver, Colorado",
+      "occupation":"Dental Assitant",
+      "status":    "Winner"
+    },
+    {
+      "name":       "Sam Grover",
+      "age":        "29",
+      "hometown":   "New York, New York",
+      "occupation": "Entertainer",
+      "status":     "Eliminated Week 6"
+    }
+  ]
+}
+
 def get_first_name_of_season_winner(data, season)
   data.each do |season_number, contestant_data|
     if season_number == season
@@ -9,7 +44,7 @@ def get_first_name_of_season_winner(data, season)
   end
 end
 
-
+get_contestant_name(data, "season 30")
 
 def get_contestant_name(data, occupation)
   # code here
