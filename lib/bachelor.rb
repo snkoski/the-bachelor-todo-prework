5ab9a27bed2d36c6551bc4735b8f63461659246b
Shawn Koski <shawnnkoski@gmail.com>
2018-04-07 01:16:08 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -5,6 +5,7 @@ require 'pry'
 def get_first_name_of_season_winner(data, season)
   data.each do |season_number, contestant_data|
     if  season.to_s == season_number.to_s
+      return "Season test"
       #binding.pry
       contestant_data.each do |contestant|
         if contestant[:status] == "Winner"
@@ -17,7 +18,7 @@ def get_first_name_of_season_winner(data, season)
       end
     end
   end
-  retrun "test"
+  #retrun "test"
 end
 
 
