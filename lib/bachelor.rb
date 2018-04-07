2ba121ce2a15ed18fdcb069649c1bffafec5f23b
Shawn Koski <shawnnkoski@gmail.com>
2018-04-07 00:40:56 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,39 +1,5 @@
 require 'pry'
 
-data = { 
-  "season 30": [
-    {
-      "name":      "Beth Smalls",
-      "age":       "26",
-      "hometown":  "Great Falls, Virginia",
-      "occupation":"Nanny/Freelance Journalist",
-      "status":    "Winner"
-    },
-    {
-      "name":       "Becca Tilley",
-      "age":        "27",
-      "hometown":   "Shreveport, Louisiana",
-      "occupation": "Chiropractic Assistant",
-      "status":     "Eliminated Week 8"
-    }
-  ],
-  "season 29": [
-    {
-      "name":      "Ashley Yeats",
-      "age":       "24",
-      "hometown":  "Denver, Colorado",
-      "occupation":"Dental Assitant",
-      "status":    "Winner"
-    },
-    {
-      "name":       "Sam Grover",
-      "age":        "29",
-      "hometown":   "New York, New York",
-      "occupation": "Entertainer",
-      "status":     "Eliminated Week 6"
-    }
-  ]
-}
 
 def get_first_name_of_season_winner(data, season)
   first_name = ""
