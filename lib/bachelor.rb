539140c957467941d331ef13836146866e84fbb2
Shawn Koski <shawnnkoski@gmail.com>
2018-04-07 01:13:22 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,10 +1,11 @@
-#require 'pry'
+require 'pry'
 
 
 
 def get_first_name_of_season_winner(data, season)
   data.each do |season_number, contestant_data|
     if  season == season_number.to_s
+      binding.pry
       contestant_data.each do |contestant|
         if contestant[:status] == "Winner"
           first_name = contestant[:name].split
