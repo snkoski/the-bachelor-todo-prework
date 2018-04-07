20238bb5416311804a7c1eebe9f4a7f7e11b5933
Shawn Koski <shawnnkoski@gmail.com>
2018-04-07 00:27:08 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -37,7 +37,7 @@ data = {
 
 def get_first_name_of_season_winner(data, season)
   data.each do |season_number, contestant_data|
-    if season_number == season
+    
      puts contestant_data[0]
       binding.pry
     end
